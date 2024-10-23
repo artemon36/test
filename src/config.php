dbf0747e88183dcea8642004f817882dbf91370b
Tim Qwees <88802215+artemon36@users.noreply.github.com>
2024-10-23 19:30:59 +0300

    Update config.php

--- src/config.php
+++ src/config.php
@@ -3,5 +3,5 @@
 const DB_HOST = 'server127.hosting.reg.ru'; /*имя хостинга*/
 const DB_PORT = '3306'; /*порт хостинга */
 const DB_NAME = 'u2241890_auth'; /* имя папки базы данных */
-const DB_USERNAME = 'u2241890_MandoBD'; /* логин от БД */
-const DB_PASSWORD = 'mandomemori2022'; /* Пароль от БД */
+const DB_USERNAME = '....'; /* логин от БД */
+const DB_PASSWORD = '.....'; /* Пароль от БД */
